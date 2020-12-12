a88aaeceb1c75cfe05a1a8f542d902c0412cdb5a
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-12-13 00:21:31 +0900

    not just a string but MasawadaSymbol

--- src/components/MasawadaSlot2/Slotmachine/types.ts
+++ src/components/MasawadaSlot2/Slotmachine/types.ts
@@ -1,7 +1,7 @@
 export const masawadaSymbols = ["ma", "sa", "wa", "da"] as const;
 export type MasawadaSymbol = typeof masawadaSymbols[number];
-export type IncompleteSlotItem = string | undefined;
+export type IncompleteSlotItem = MasawadaSymbol | undefined;
 export type IncompleteSlots = Array<IncompleteSlotItem>;
-export type StoppedSlotItem = string;
+export type StoppedSlotItem = MasawadaSymbol;
 export type StoppedSlots = Array<StoppedSlotItem>;
 export type Slots = StoppedSlots | IncompleteSlots;
