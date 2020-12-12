1d28cb58463c376fc099b7623b873eac10a545c7
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-12-13 01:30:33 +0900

    2

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 export const Footer: React.FC = () => (
   <address>
-    masawada slot by <a href="https://github.com/astj">astj</a>. more details at{" "}
-    <a href="https://blog.astj.space/entry/2020/12/06/235824">
-      ma→sa→wa→da で masawada 完成 (masawada slotmachine 第1回) - 平常運転
-    </a>
+    masawada slot by <a href="https://github.com/astj">astj</a>. more details at
+    <ul>
+      <li>
+        <a href="https://blog.astj.space/entry/2020/12/06/235824">
+          ma→sa→wa→da で masawada 完成 (masawada slotmachine 第1回) - 平常運転
+        </a>
+      </li>
+      <li>
+        <a href="https://blog.astj.space/entry/2020/12/13/012539">
+          マ→サ→ワ→ダでマサワダ完成 (masawada slotmachine 第2回) - 平常運転
+        </a>
+      </li>
+    </ul>
   </address>
 );
